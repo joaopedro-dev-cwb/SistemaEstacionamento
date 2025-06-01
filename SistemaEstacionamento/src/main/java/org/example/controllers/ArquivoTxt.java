e60748ed04bef798d7d9463b6bbd700e137ef306
GabrieLaureanoUP <gabriel.silva0@cs.up.edu.br>
2025-06-01 14:42:35 -0300

    arquivo

--- SistemaEstacionamento/src/main/java/org/example/controllers/ArquivoTxt.java
+++ SistemaEstacionamento/src/main/java/org/example/controllers/ArquivoTxt.java
@@ -1,8 +1,9 @@
 package org.example.controllers;
 
 import org.example.model.Veiculo;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
