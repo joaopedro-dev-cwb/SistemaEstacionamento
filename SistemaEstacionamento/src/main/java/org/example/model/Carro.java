3a0e66798e51ec19b6f76fff9d20930f2bc23004
GabrieLaureanoUP <gabriel.silva0@cs.up.edu.br>
2025-06-01 13:24:14 -0300

    Metodo de Sobrecarga

--- SistemaEstacionamento/src/main/java/org/example/model/Carro.java
+++ SistemaEstacionamento/src/main/java/org/example/model/Carro.java
@@ -7,7 +7,7 @@ public class Carro extends Veiculo {
     private static final double VALOR_HORA = 10.0;
 
     public Carro(String placa, String modelo, String cor, LocalDateTime dataHoraEntrada) {
-        super(placa, modelo, cor, dataHoraEntrada);
+        super(placa, modelo, cor);
     }
 
     @Override
@@ -15,4 +15,13 @@ public class Carro extends Veiculo {
         return VALOR_HORA;
     }
 
+    @Override
+    public String toString() {
+        return "Carro{"
+                + "placa='" + getPlaca() + '\''
+                + ", modelo='" + getModelo() + '\''
+                + ", cor='" + getCor() + '\''
+                + ", dataHoraEntrada=" + getDataHoraEntrada()
+                + '}';
+    }
 }
--- SistemaEstacionamento/src/main/java/org/example/model/Moto.java
+++ SistemaEstacionamento/src/main/java/org/example/model/Moto.java
@@ -7,11 +7,21 @@ public class Moto extends Veiculo {
     private static final double VALOR_HORA = 5.0;
 
     public Moto(String placa, String modelo, String cor, LocalDateTime dataHoraEntrada) {
-        super(placa, modelo, cor, dataHoraEntrada);
+        super(placa, modelo, cor);
     }
 
     @Override
     public double getValorPorHoras() {
         return VALOR_HORA;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto{"
+                + "placa='" + getPlaca() + '\''
+                + ", modelo='" + getModelo() + '\''
+                + ", cor='" + getCor() + '\''
+                + ", dataHoraEntrada=" + getDataHoraEntrada()
+                + '}';
     }
 }
