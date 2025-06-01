dd0ee3f76943712d7a004e6b284dc86468c88f7e
GabrieLaureanoUP <gabriel.silva0@cs.up.edu.br>
2025-06-01 14:48:15 -0300

    data do log

--- SistemaEstacionamento/src/main/java/org/example/util/Log.java
+++ SistemaEstacionamento/src/main/java/org/example/util/Log.java
@@ -1,15 +1,27 @@
 package org.example.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public abstract class Log {
-    public static void setError(){
+
+    public static void setError() {
         try {
             File logDir = new File("logs");
             logDir.mkdir();
 
-            PrintStream logErro = new PrintStream("logs/erro.txt");
+            PrintStream logErro = new PrintStream(new FileOutputStream("logs/erro.txt", true)) {
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+                @Override
+                public void println(String x) {
+                    super.println("[" + LocalDateTime.now().format(dtf) + "] " + x);
+                }
+            };
+
             System.setErr(logErro);
         } catch (Exception e) {
             System.err.println(e.getMessage());
