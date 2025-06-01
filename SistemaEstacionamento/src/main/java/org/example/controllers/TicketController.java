225c6d1fe6566421997d129b6626b80c9186d2ba
Joao Pedro <j.pedrowb@gmail.com>
2025-06-01 12:01:07 -0300

    correções ticket controller

--- SistemaEstacionamento/src/main/java/org/example/controllers/TicketController.java
+++ SistemaEstacionamento/src/main/java/org/example/controllers/TicketController.java
@@ -1,30 +1,35 @@
 package org.example.controllers;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.example.factory.TicketFactory;
 import org.example.model.Ticket;
 import org.example.model.Vaga;
 import org.example.model.Veiculo;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 public class TicketController {
 
     private List<Ticket> tickets;
 
-    TicketController() {
+    public TicketController() {
         this.tickets = new ArrayList<>();
     }
 
-    public List<Ticket> gerarTicket(Veiculo veiculo) {
+    public Ticket gerarTicket(Veiculo veiculo, Vaga vaga, double valor) {
         int id = 1;
         if (!tickets.isEmpty()) {
-            id = tickets.getLast().getId() + 1;
+            id = tickets.get(tickets.size() - 1).getId() + 1;
         }
-        Vaga vaga = veiculo.;
         LocalDateTime dataHoraEntrada = veiculo.getDataHoraEntrada();
-        Ticket ticket = TicketFactory.criarTicket(id, veiculo, vaga, dataHoraEntrada, LocalDateTime.now(), valor);
+        LocalDateTime dataHoraSaida = LocalDateTime.now();
+        Ticket ticket = TicketFactory.criarTicket(id, veiculo, vaga, dataHoraEntrada, dataHoraSaida, valor);
+        tickets.add(ticket);
+        return ticket;
     }
 
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
 }
