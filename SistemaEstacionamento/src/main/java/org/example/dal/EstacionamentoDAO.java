45678ad906179cb47d5fe3479ccd18c58c27eefe
Marcos Maues <yboostx@gmail.com>
2025-06-05 20:25:19 -0300

    eu

---
+++ SistemaEstacionamento/src/main/java/org/example/dal/EstacionamentoDAO.java
@@ -0,0 +1,37 @@
+package org.example.dal;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.example.model.Veiculo;
+
+public class EstacionamentoDAO {
+
+    private static final String CAMINHO = "src/main/dados";
+
+    public static void salvar(List<Veiculo> veiculos) throws IOException {
+        File diretorio = new File(CAMINHO);
+        diretorio.mkdirs();
+
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(CAMINHO + "/veiculos.ser"))) {
+            oos.writeObject(veiculos);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<Veiculo> carregar () throws IOException, ClassNotFoundException {
+
+        File arquivo = new File(CAMINHO + "/veiculos.ser");
+        if(!arquivo.exists()) return new ArrayList<>();
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivo))){
+            return (List<Veiculo>) ois.readObject();
+        } 
+    }
+}
