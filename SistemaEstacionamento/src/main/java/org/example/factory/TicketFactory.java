b4e040f60e2507b3858113fd1cbbf88532282ec7
Marcos Maues <yboostx@gmail.com>
2025-06-10 17:45:35 -0300

    implementação de verificação ticketFactory

--- SistemaEstacionamento/src/main/java/org/example/factory/TicketFactory.java
+++ SistemaEstacionamento/src/main/java/org/example/factory/TicketFactory.java
@@ -8,6 +8,35 @@ import org.example.model.Veiculo;
 
 public class TicketFactory {
     public static Ticket criarTicket(int id, Veiculo veiculo, Vaga vaga, LocalDateTime dataHoraEntrada, LocalDateTime dataHoraSaida, double valor)throws Exception {
+        if (veiculo == null) {
+            throw new IllegalArgumentException("Veículo não pode ser nulo");
+        }
+
+        if (vaga == null) {
+            throw new IllegalArgumentException("Vaga não pode ser nula");
+        }
+
+        if (dataHoraEntrada == null) {
+            throw new IllegalArgumentException("Data e hora de entrada não podem ser nulas");
+        }   
+
+        if (dataHoraSaida == null) {
+            throw new IllegalArgumentException("Data e hora de saída não podem ser nulas");
+        }
+
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor do ticket deve ser maior que zero.");
+        }
+
+        if (id <= 0) {
+            throw new IllegalArgumentException("ID do ticket deve ser maior que zero.");
+        }
+
+        if (dataHoraSaida.isBefore(dataHoraEntrada)) {
+            throw new IllegalArgumentException("Data e hora de saída não podem ser anteriores à data e hora de entrada.");
+        }
+
+
         try {
             return new Ticket(id, veiculo, vaga, dataHoraEntrada, dataHoraSaida, valor);
         } catch (Exception e) {
